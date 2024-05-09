032bda7d4817ef17405106ed4496e8072e01c1b7
nalule-grace <g.nalule@alustudent.com>
2024-05-09 15:10:49 +0200

    buildnrun

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 dotnet new console -n 2-new_project
 cd 2-new_project
-echo 'Console.WriteLine("Hello World!");'>program.cs
+echo 'Console.WriteLine("Hello World!");'>Program.cs
 dotnet build
 dotnet run

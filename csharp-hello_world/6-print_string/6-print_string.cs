b17b2347842319dba03e33d4be9800b3220ce04f
nalule-grace <g.nalule@alustudent.com>
2024-05-09 19:10:23 +0200

    files

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine(string.Concat(Enumerable.Repeat(str,3)));
+    Console.WriteLine(str.Substring(0,9));
+        }
+}
---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine($"Percent: {(percent * 100):N2}%");
+        Console.WriteLine($"Currency: {string.Format("{0:C2}", currency)}");
+	}
+}

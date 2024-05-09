5450fd537b1f5d96f1b00ccece0660e61a23305a
nalule-grace <g.nalule@alustudent.com>
2024-05-09 19:34:11 +0200

    dotnett

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 
 class Program
 {
@@ -6,7 +6,7 @@ class Program
 	{
 		string str1 = "Holberton";
 		string str2 = "School";
-		str1 += " " str2;
+		str1 += " " +str2;
 		Console.WriteLine("Welcome to {0}!", str1);
 	}
 }

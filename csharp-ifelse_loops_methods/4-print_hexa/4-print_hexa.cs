6fb657ca041e36003ad7e8394149abf98f4f4ab9
nalule-grace <g.nalule@alustudent.com>
2024-05-11 10:50:51 +0200

    hexa

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -6,7 +6,7 @@ class Program
     {
         for(int i = 0; i<99; i++) 
         {
-            Console.Write($"{i} = 0x{i.ToString("x")} \n");
+            Console.Write($"{i} = 0x{i.ToString("x")}");
         }
     }
 }

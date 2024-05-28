f7e264313809591ef40aeb59035676f486036c36
nalule-grace <g.nalule@alustudent.com>
2024-05-28 21:12:01 +0200

    corr

--- csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int [,] myArray = new int[5,5];
+        for (int i = 0; i<myArray.GetLength(0); i++)
+        {
+            for (int j = 0; j<myArray.GetLength(1); j++)
+            {
+                myArray[i,j] = 0;
+            }
+        }
+        myArray[2,2] = 1;
+
+        for (int i = 0; i<myArray.GetLength(0); i++)
+        {
+            for (int j = 0; j<myArray.GetLength(1); j++)
+            {
+                if (j != myArray.GetLength(1) - 1)
+                {
+                    Console.Write(myArray[i,j] + " ");
+                }else{
+                    Console.Write(myArray[i,j]);
+                }
+            }
+            Console.WriteLine();
+        }
+
+    }
+}

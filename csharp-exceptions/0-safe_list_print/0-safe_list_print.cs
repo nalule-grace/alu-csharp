92ee4065bffc3d727f9096054ecb0fa19859eb49
nalule-grace <g.nalule@alustudent.com>
2024-06-06 19:33:44 +0200

    exception handling

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+        try
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+                count++;
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            // Catch the exception if n is greater than the length of the list
+        }
+        return count;
+    }
+}
+
+/*class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> myList = new List<int>() { 1, 2, 3, 4, 5 };
+        int count;
+
+        count = List.SafePrint(myList, myList.Count);
+        Console.WriteLine("Number of integers printed: " + count);
+        Console.WriteLine();
+
+        count = List.SafePrint(myList, myList.Count - 2);
+        Console.WriteLine("Number of integers printed: " + count);
+        Console.WriteLine();
+
+        count = List.SafePrint(myList, myList.Count + 2);
+        Console.WriteLine("Number of integers printed: " + count);
+    }
+*/
---
+++ csharp-exceptions/README.md
@@ -0,0 +1,7 @@
+THIS IS PEOJECT IS ABPUT EXCEPTIONG HANDLING
+What are exceptions and when to use them
+What is the purpose of catching exceptions
+How to handle exceptions
+How to use try, catch, finally
+How to raise built-in exceptions
+When to implement clean-up actions after exceptions

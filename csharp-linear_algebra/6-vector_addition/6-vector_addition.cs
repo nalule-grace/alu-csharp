614e901d75410cb46d3eb0322fec87da3b97888e
nalule-grace <g.nalule@alustudent.com>
2024-07-17 17:54:19 +0200

    addvectors

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,20 @@
+﻿public class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != vector2.Length || (vector1.Length != 2 && vector1.Length != 3))
+        {
+        return new double[] { -1, -1, -1 }; // Invalid vectors
+        }
+
+        int dimension = vector1.Length;
+        double[] result = new double[dimension];
+
+        for (int i = 0; i < dimension; i++)
+        {
+        result[i] = vector1[i] + vector2[i];
+        }
+
+        return result;
+    }
+}

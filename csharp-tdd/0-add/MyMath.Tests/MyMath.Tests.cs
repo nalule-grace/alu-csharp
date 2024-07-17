17b68882cf691ac85ccacd57355b87bd4b2cb5d3
nalule-grace <g.nalule@alustudent.com>
2024-07-17 15:10:20 +0200

    pythagoras

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -16,6 +16,7 @@ public class Tests
     //}
     [TestCase (2,3,5)]
     [TestCase (-1,-2,-3)]
+    [TestCase(1,1,2)]
     public void Test_Addition(int a,int b, int result){
         Assert.That(result, Is.EqualTo(MyMath.Operations.Add(a,b)));
     }

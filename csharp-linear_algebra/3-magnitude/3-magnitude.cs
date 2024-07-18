c6fb48367aca2a40a11ac17e2eec6e8c3ef0c8f6
nalule-grace <g.nalule@alustudent.com>
2024-07-18 21:44:41 +0200

    comment

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,5 +1,13 @@
-﻿public class VectorMath
+﻿/// <summary>
+/// vector magnitude
+/// </summary>
+public class VectorMath
 {
+    /// <summary>
+/// Calculates the magnitude (Euclidean norm) of a 2D or 3D vector.
+/// </summary>
+/// <param name="vector">The vector for which to calculate the magnitude.</param>
+/// <returns>The magnitude of the vector, or -1 if the vector dimension is not 2 or 3.</returns>
     public static double Magnitude(double[] vector)
     {
         if (vector.Length != 2 && vector.Length != 3)

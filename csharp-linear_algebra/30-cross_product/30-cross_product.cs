71da9f09f48e2ee9556906b679500f69a1d41678
nalule-grace <g.nalule@alustudent.com>
2024-07-18 22:34:20 +0200

    comment

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -1,5 +1,17 @@
-﻿public class VectorMath
+﻿/// <summary>
+/// This class contains methods for performing operations on vectors.
+/// </summary>
+public class VectorMath
 {
+     /// <summary>
+    /// Calculates the cross product of two 3D vectors.
+    /// </summary>
+    /// <param name="vector1">The first 3D vector.</param>
+    /// <param name="vector2">The second 3D vector.</param>
+    /// <returns>
+    /// The resulting cross product vector, or a vector filled with -1 if the input vectors are not 3D.
+    /// </returns>
+    /// <exception cref="ArgumentException">Thrown if either input vector is not of dimension 3.</exception>
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
         if (vector1.Length != 3 || vector2.Length != 3)

28d8138ce38bd8bd01345562101d7bf3bdf4560e
nalule-grace <g.nalule@alustudent.com>
2024-07-18 20:45:15 +0200

    vectorscalar

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,17 @@
+﻿public class VectorMath{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length !=2 && vector.Length !=3){
+            return new double[] {-1, -1, -1};
+        }
+        int dimension = vector.Length;
+        double[] result = new double[dimension];
+
+        for (int i = 0; i < dimension; i++)
+        {
+        result[i] = vector[i] * scalar;
+        }
+
+        return result;
+    }
+}

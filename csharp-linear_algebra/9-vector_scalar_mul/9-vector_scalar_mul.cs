faa249d7824312c6168ca881d16993371dcbc596
nalule-grace <g.nalule@alustudent.com>
2024-07-18 21:49:03 +0200

    comment

--- csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -2,6 +2,12 @@
 ///THIS class contains methods for performing vector operations
 /// </summary>
 public class VectorMath{
+    /// <summary>
+/// Performs element-wise multiplication of a vector by a scalar value.
+/// </summary>
+/// <param name="vector">The vector to be multiplied.</param>
+/// <param name="scalar">The scalar value to multiply by.</param>
+/// <returns>A new vector containing the product of each element in the vector and the scalar.</returns>
     public static double[] Multiply(double[] vector, double scalar)
     {
         if (vector.Length !=2 && vector.Length !=3){

dbce79523ba932b1f1015d9a6be8a0290f64b265
nalule-grace <g.nalule@alustudent.com>
2024-07-18 21:38:51 +0200

    comment

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,8 +1,15 @@
 ﻿/// <summary>
-/// vector addtion
+/// Adds corresponding elements of two vectors
 /// </summary>
 public class VectorMath
 {
+
+    /// <summary>
+    /// Adds corresponding elements of two vectors.
+    /// </summary>
+    /// <param name="vector1">The first vector.</param>
+    /// <param name="vector2">The second vector.</param>
+    /// <returns>A new vector containing the element-wise sum.</returns>
     public static double[] Add(double[] vector1, double[] vector2)
     {
         if (vector1.Length != vector2.Length || (vector1.Length != 2 && vector1.Length != 3))

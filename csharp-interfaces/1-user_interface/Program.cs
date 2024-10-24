1c74b61ba7307db495fe6fdd6ff14443607d5cdc
nalule-grace <g.nalule@alustudent.com>
2024-10-24 14:56:42 +0200

    interfaces

---
+++ csharp-interfaces/1-user_interface/Program.cs
@@ -0,0 +1,104 @@
+﻿﻿/// <summary>
+/// Base Class
+/// </summary>
+public abstract class Base {
+    /// <summary>
+    /// The public property name
+    /// </summary>
+    public string ? name {get; set;}
+
+    /// <summary>
+    /// Override for the ToString()
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+/// <summary>
+/// IInterface for interactivity
+/// </summary>
+
+public interface IInteractive {
+    /// <summary>
+    /// Method to Interact
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// IBreakable interface for durability
+/// </summary>
+
+public interface IBreakable {
+    /// <summary>
+    /// Public property durability
+    /// </summary>
+    int durability {get; set;}
+
+    /// <summary>
+    /// Break method 
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// ICollectable Interface
+/// </summary>
+public interface ICollectable {
+    /// <summary>
+    /// Bool property isCollected
+    /// </summary>
+    bool isCollected {get; set;}
+
+    /// <summary>
+    /// Collect method
+    /// </summary>
+    void Collect() {
+
+    }
+}
+
+/// <summary>
+/// TestObject to test the functionalities
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable {
+    /// <summary>
+    /// Interact implementation
+    /// </summary>
+    public void Interact() {
+
+    }
+
+    /// <summary>
+    /// Interact implementation
+    /// </summary>
+    public void Break() {
+
+    }
+
+    /// <summary>
+    /// Durability implementation
+    /// </summary>
+    public int durability {
+        get {return durability;}
+        set {durability = value;}
+    }
+
+    /// <summary>
+    /// Collect implementation
+    /// </summary>
+    public void Collect() {
+
+    }
+
+    /// <summary>
+    /// Collect Implementation
+    /// </summary>
+    public bool isCollected {
+        get {return isCollected;}
+        set {isCollected = value;}
+    }
+
+}
---
+++ csharp-interfaces/3-decorations/Program.cs
@@ -0,0 +1,185 @@
+﻿﻿using System.Dynamic;
+
+/// <summary>
+/// Base Class
+/// </summary>
+public abstract class Base {
+    /// <summary>
+    /// The public property name
+    /// </summary>
+    public string ? name {get; set;}
+
+    /// <summary>
+    /// Override for the ToString()
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+/// <summary>
+/// IInterface for interactivity
+/// </summary>
+
+public interface IInteractive {
+    /// <summary>
+    /// Method to Interact
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// IBreakable interface for durability
+/// </summary>
+
+public interface IBreakable {
+    /// <summary>
+    /// Public property durability
+    /// </summary>
+    public int durability {get; set;}
+
+    /// <summary>
+    /// Break method 
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// ICollectable Interface
+/// </summary>
+public interface ICollectable {
+    /// <summary>
+    /// Bool property isCollected
+    /// </summary>
+    bool isCollected {get; set;}
+
+    /// <summary>
+    /// Collect method
+    /// </summary>
+    void Collect() {
+
+    }
+}
+
+// /// <summary>
+// /// TestObject to test the functionalities
+// /// </summary>
+// public class TestObject : Base, IInteractive, IBreakable, ICollectable {
+//     /// <summary>
+//     /// Interact implementation
+//     /// </summary>
+//     public void Interact() {
+
+//     }
+
+//     /// <summary>
+//     /// Interact implementation
+//     /// </summary>
+//     public void Break() {
+
+//     }
+
+//     /// <summary>
+//     /// Durability implementation
+//     /// </summary>
+//     public int durability {
+//         get {return durability;}
+//         set {durability = value;}
+//     }
+
+//     /// <summary>
+//     /// Collect implementation
+//     /// </summary>
+//     public void Collect() {
+
+//     }
+
+//     /// <summary>
+//     /// Collect Implementation
+//     /// </summary>
+//     public bool isCollected {
+//         get {return isCollected;}
+//         set {isCollected = value;}
+//     }
+
+// }
+
+/// <summary>
+/// The Door Class
+/// </summary>
+public class Door : Base, IInteractive {
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Door(string name = "Door") {
+        this.name = name;
+    }
+
+    /// <summary>
+    /// Interact implementation
+    /// </summary>
+    public void Interact() {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
+
+/// <summary>
+/// Decoration Class
+/// </summary>
+public class Decoration : Base, IInteractive, IBreakable {
+
+    /// <summary>
+    /// Public Bool isQuestItem
+    /// </summary>
+    public bool isQuestItem;
+
+    /// <summary>
+    /// Durability implememntation
+    /// </summary>
+    public int durability {get; set;}
+
+    /// <summary>
+    /// Decoration Constructor
+    /// </summary>
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false) {
+        this.name = name;
+        if (durability <= 0) {
+            throw new Exception("Durability must be greater than 0");
+        } else {
+            this.durability = durability;
+        }
+        this.isQuestItem = isQuestItem;
+    }
+
+    /// <summary>
+    /// Interact implementation
+    /// </summary>
+
+    public void Interact() {
+        if (durability <= 0) {
+            Console.WriteLine($"The {name} has been broken.");
+        } else {
+            if (isQuestItem) {
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+            } 
+            else {
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Break implementation
+    /// </summary>
+    public void Break() {
+        durability--;
+        if (durability > 0) {
+            Console.WriteLine($"You hit the {name}. It cracks.");
+        } else if (durability == 0) {
+            Console.WriteLine($"You smash the {name}. What a mess.");
+        } else {
+            Console.WriteLine($"The {name} is already broken.");
+        }
+    }
+}

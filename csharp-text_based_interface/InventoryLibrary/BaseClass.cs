0b6193fb3282917ac31e9d7c361f336664859da1
nalule-grace <g.nalule@alustudent.com>
2024-10-30 23:09:19 +0200

    textbased

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace InventoryLibrary;
+
+/// <summary>
+/// The BaseClass
+/// </summary>
+public class BaseClass
+{
+    /// <summary>
+    /// Id
+    /// </summary>
+    public string id;
+    /// <summary>
+    /// The Date Created
+    /// </summary>
+    public DateTime date_created;
+    /// <summary>
+    /// The date Updated
+    /// </summary>
+    public DateTime date_updated;
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public BaseClass(){
+        id = Guid.NewGuid().ToString();
+        date_created = DateTime.Now;
+        date_updated = DateTime.Now;
+    }
+
+}
---
+++ csharp-text_based_interface/InventoryLibrary/Inventory.cs
@@ -0,0 +1,39 @@
+﻿namespace InventoryLibrary;
+
+/// <summary>
+/// The Inventory class
+/// </summary>
+public class Inventory: BaseClass {
+    /// <summary>
+    /// UserId
+    /// </summary>
+    public string UserId {get; set;}
+    /// <summary>
+    /// ItemId
+    /// </summary>
+    public string ItemId {get; set;}
+    private int _quantity;
+    /// <summary>
+    /// Quantity
+    /// </summary>
+    public int Quantity {
+        get => _quantity;
+        set {
+            if (value < 0) {
+                throw new ArgumentOutOfRangeException("Quantity can't be negative");
+            }
+            _quantity = 0;
+        }
+    }
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="uid"></param>
+    /// <param name="iid"></param>
+    /// <param name="qty"></param>
+    public Inventory(string uid, string iid, int qty) {
+        UserId = uid;
+        ItemId = iid;
+        Quantity = qty;
+    }
+}
